d6c0f1a2836a1e173cce2a4fd1fbfe83af15b164
jaecheol sim <wocjf0513@naver.com>
2023-12-22 16:12:08 +0900

    feature: Member Controller 생성

---
+++ src/main/java/com/yanolja/scbj/domain/member/controller/MemberAuthRestController.java
@@ -0,0 +1,28 @@
+package com.yanolja.scbj.domain.member.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+@RequestMapping("/api/token")
+public class MemberAuthRestController {
+
+    private final MemberAuthService memberAuthService;
+
+    MemberAuthRestController(MemberAuthService memberAuthService) {
+        this.memberAuthService = memberAuthService;
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<TokenResponse> refreshAccessToken(
+        @RequestBody RefreshRequest refreshRequest) {
+        log.info("accessToken:{}, refreshToken:{}",refreshRequest.getAccessToken(), refreshRequest.getRefreshToken());
+        return ResponseEntity.ok().body(memberAuthService.refreshAccessToken(refreshRequest));
+    }
+
+}
