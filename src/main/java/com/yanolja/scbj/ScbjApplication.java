28bbe9f1a117309544e40e8e06033ad51371d287
jaecheol sim <wocjf0513@naver.com>
2023-12-22 15:54:38 +0900

    first: 기본 프로젝트 생성

---
+++ src/main/java/com/yanolja/scbj/ScbjApplication.java
@@ -0,0 +1,13 @@
+package com.yanolja.scbj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScbjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScbjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yanolja/scbj/ScbjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanolja.scbj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScbjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
