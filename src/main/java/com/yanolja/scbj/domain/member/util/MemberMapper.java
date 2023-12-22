313c59c87b52b22a1a3ff18b73b48295a1a67b38
jaecheol sim <wocjf0513@naver.com>
2023-12-22 16:19:05 +0900

    feature: Member Mapper 생성

---
+++ src/main/java/com/yanolja/scbj/domain/member/util/MemberMapper.java
@@ -0,0 +1,58 @@
+package com.yanolja.scbj.domain.member.util;
+
+import java.util.Collections;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class MemberMapper {
+
+
+    public static Member toMember(MemberSignUpRequest signUpMemberRequest, String encodePassword) {
+        return Member.builder()
+            .email(signUpMemberRequest.email())
+            .password(encodePassword)
+            .phone(signUpMemberRequest.phone())
+            .name(signUpMemberRequest.name())
+            .authority(Authority.ROLE_USER)
+            .build();
+    }
+
+    public static MemberResponse toMemberResponse(Member member) {
+        return MemberResponse.builder()
+            .id(member.getId())
+            .email(member.getEmail())
+            .name(member.getName())
+            .phone(member.getPhone())
+            .build();
+    }
+
+    public static TokenResponse toTokenResponse(String accessToken, String refreshToken) {
+        return TokenResponse.builder()
+            .accessToken(accessToken)
+            .refreshToken(refreshToken)
+            .build();
+    }
+
+    public static MemberSignInResponse toSignInResponse(MemberResponse memberResponse,
+        TokenResponse tokenResponse) {
+        return MemberSignInResponse.builder()
+            .memberResponse(memberResponse)
+            .tokenResponse(tokenResponse)
+            .build();
+    }
+
+    public static UserDetails toUserDetails(Member member) {
+        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(
+            member.getAuthority().toString());
+        return new User(
+            String.valueOf(member.getId()),
+            member.getPassword(),
+            Collections.singleton(grantedAuthority));
+    }
+
+}
