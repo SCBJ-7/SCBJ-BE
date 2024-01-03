3d1dc5b92d0201ae37077a55f3050790a48d0447
Aleexender <alexkjh2011@gmail.com>
2024-01-03 21:25:33 +0900

    fix: Application 클래스 수정

--- src/main/java/com/yanolja/scbj/ScbjApplication.java
+++ src/main/java/com/yanolja/scbj/ScbjApplication.java
@@ -2,14 +2,20 @@ package com.yanolja.scbj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing 
 public class ScbjApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ScbjApplication.class, args);
     }
 
+
+
 }
+
+@Configuration
+@EnableJpaAuditing
+class ApplicationConfig {}
