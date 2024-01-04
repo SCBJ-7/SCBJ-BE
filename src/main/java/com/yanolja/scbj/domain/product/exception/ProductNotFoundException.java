ed771de7807123423b148cf2b2cca9106ecba644
kwonMInwoo <qwerty029369@naver.com>
2024-01-04 10:42:44 +0900

    feat: 상품 not found 예외 클래스 추가
    - Product Not Found Exception 클래스 추가

--- src/main/java/com/yanolja/scbj/domain/product/exception/ProductNotFoundException.java
+++ src/main/java/com/yanolja/scbj/domain/product/exception/ProductNotFoundException.java
@@ -1,2 +1,13 @@
-package com.yanolja.scbj.domain.product.exception;public class ProductNotFoundException {
+package com.yanolja.scbj.domain.product.exception;
+
+import com.yanolja.scbj.global.exception.ApplicationException;
+import com.yanolja.scbj.global.exception.ErrorCode;
+
+public class ProductNotFoundException extends ApplicationException {
+
+    public ProductNotFoundException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+
 }
