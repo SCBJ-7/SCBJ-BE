3f75d01bca8c60b4e5ab7468516c14d810632014
Yurim Yang <yang980329@likelion.org>
2024-01-04 14:41:01 +0900

    feat : 상품 조회 api와 merging 진행 - 민우님 코드와 merging중 재 update

---
+++ src/main/java/com/yanolja/scbj/domain/product/dto/ProductFindResponse.java
@@ -0,0 +1,50 @@
+package com.yanolja.scbj.domain.product.dto;
+
+import com.yanolja.scbj.domain.hotelRoom.dto.response.RoomThemeFindResponse;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ProductFindResponse {
+
+    private String hotelName;
+    private String roomName;
+    private LocalDateTime checkIn;
+    private LocalDateTime checkOut;
+    private int originalPrice;
+    private int sellingPrice;
+    private int standardPeople;
+    private int maxPeople;
+    private String bedType;
+    private RoomThemeFindResponse roomTheme;
+    private String hotelAddress;
+    private String hotelInfoUrl;
+    private boolean saleStatus;
+
+    @Builder
+    private ProductFindResponse(String hotelName, String roomName, LocalDateTime checkIn,
+        LocalDateTime checkOut, int originalPrice, int sellingPrice, int standardPeople,
+        int maxPeople,
+        String bedType, RoomThemeFindResponse roomTheme, String hotelAddress, String hotelInfoUrl,
+        boolean saleStatus) {
+        this.hotelName = hotelName;
+        this.roomName = roomName;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+        this.originalPrice = originalPrice;
+        this.sellingPrice = sellingPrice;
+        this.standardPeople = standardPeople;
+        this.maxPeople = maxPeople;
+        this.bedType = bedType;
+        this.roomTheme = roomTheme;
+        this.hotelAddress = hotelAddress;
+        this.hotelInfoUrl = hotelInfoUrl;
+        this.saleStatus = saleStatus;
+    }
+}
