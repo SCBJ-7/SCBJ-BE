8cc5de26a4a974b0ffc31b4d42b492d4a6e9f84d
Yurim Yang <yang980329@likelion.org>
2024-01-06 11:01:41 +0900

    feat: 예약내역 조회 api 중 리팩토링용 converter 생성
    - 리팩토링시 사용할 converter 생성

---
+++ src/main/java/com/yanolja/scbj/domain/reservation/service/ReservationDtoConverter.java
@@ -0,0 +1,14 @@
+package com.yanolja.scbj.domain.reservation.service;
+
+import com.yanolja.scbj.domain.reservation.dto.response.ReservationFindResponse;
+import com.yanolja.scbj.domain.reservation.entity.Reservation;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservationDtoConverter {
+
+    public ReservationFindResponse toFindResponse(Reservation reservation) {
+
+    }
+
+}
