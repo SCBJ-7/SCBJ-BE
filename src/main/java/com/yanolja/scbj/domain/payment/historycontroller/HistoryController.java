bf208afdaab4070e2318d3ecdde131ba1d76e2e0
Aleexender <alexkjh2011@gmail.com>
2024-01-08 04:57:06 +0900

    Feat: Purchased API(Controller, Service, Repository 생성

--- src/main/java/com/yanolja/scbj/domain/payment/historycontroller/HistoryController.java
+++ src/main/java/com/yanolja/scbj/domain/payment/historycontroller/HistoryController.java
@@ -1,7 +1,8 @@
 package com.yanolja.scbj.domain.payment.historycontroller;
 
 import com.yanolja.scbj.domain.payment.dto.PurchasedHistoryResponse;
-import com.yanolja.scbj.domain.payment.hisotryService.PurchasedHistoryService;
+import com.yanolja.scbj.domain.payment.dto.SaleHistoryResponse;
+import com.yanolja.scbj.domain.payment.hisotryService.HistoryService;
 import com.yanolja.scbj.global.common.ResponseDTO;
 import com.yanolja.scbj.global.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
@@ -16,19 +17,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("v1/members/purchased-history")
-public class PurchasedHistoryController {
+@RequestMapping("v1/members/")
+public class HistoryController {
 
-    private final PurchasedHistoryService purchasedHistoryService;
+    private final HistoryService historyService;
     private final SecurityUtil securityUtil;
 
-    @GetMapping
+    @GetMapping("purchased-history")
     @ResponseStatus(HttpStatus.OK)
     public ResponseDTO<Page<PurchasedHistoryResponse>> getPurchasedHistories(
         @PageableDefault(page = 1) Pageable pageable
     ) {
         Long memberId = securityUtil.getCurrentMemberId();
-        Page<PurchasedHistoryResponse> response = purchasedHistoryService.getUsersPurchasedHistory(pageable, memberId);
+        Page<PurchasedHistoryResponse> response =
+            historyService.getUsersPurchasedHistory(pageable, memberId);
+        return ResponseDTO.res(response, "조회에 성공하였습니다.");
+    }
+
+
+    @GetMapping("sale-history")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseDTO<Page<SaleHistoryResponse>> getSaleHistories(
+        @PageableDefault(page = 1) Pageable pageable
+    ) {
+        Long memberId = securityUtil.getCurrentMemberId();
+        Page<SaleHistoryResponse> response =
+            historyService.getUsersSaleHistory(pageable, memberId);
         return ResponseDTO.res(response, "조회에 성공하였습니다.");
     }
 }
