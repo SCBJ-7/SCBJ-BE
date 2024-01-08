64e93a0c82d79e3d82715ccb4c5d07b1113c387f
Aleexender <alexkjh2011@gmail.com>
2024-01-08 14:42:21 +0900

    refactor: RepostiroyTest 리팩토링 진행
    따로 메소드로 분리하여 실제 테스트 돌리는 메소드에서는 좀더 가독성이 있게 진행하였습니다!

--- src/test/java/com/yanolja/scbj/domain/payment/repository/HistoryRepositoryTest.java
+++ src/test/java/com/yanolja/scbj/domain/payment/repository/HistoryRepositoryTest.java
@@ -10,8 +10,8 @@ import com.yanolja.scbj.domain.hotelRoom.entity.RoomTheme;
 import com.yanolja.scbj.domain.member.entity.Authority;
 import com.yanolja.scbj.domain.member.entity.Member;
 import com.yanolja.scbj.domain.member.entity.YanoljaMember;
-import com.yanolja.scbj.domain.payment.dto.PurchasedHistoryResponse;
-import com.yanolja.scbj.domain.payment.dto.SaleHistoryResponse;
+import com.yanolja.scbj.domain.payment.dto.response.PurchasedHistoryResponse;
+import com.yanolja.scbj.domain.payment.dto.response.SaleHistoryResponse;
 import com.yanolja.scbj.domain.payment.entity.PaymentHistory;
 import com.yanolja.scbj.domain.product.entity.Product;
 import com.yanolja.scbj.domain.product.repository.ProductRepository;
@@ -44,6 +44,116 @@ class HistoryRepositoryTest {
     @Autowired
     private ProductRepository productRepository;
 
+    private Member createMember() {
+        Member member = Member.builder()
+            .email("user@example.com")
+            .password("password")
+            .name("홍길동")
+            .phone("010-1234-5678")
+            .authority(Authority.ROLE_USER)
+            .build();
+        entityManager.persist(member);
+        return member;
+    }
+
+    private RoomTheme createRoomTheme() {
+        RoomTheme roomTheme = RoomTheme.builder()
+            .parkingZone(true)
+            .breakfast(true)
+            .pool(true)
+            .oceanView(false)
+            .build();
+        entityManager.persist(roomTheme);
+        return roomTheme;
+    }
+
+    private Hotel createHotel(RoomTheme roomTheme) {
+        Room room = Room.builder()
+            .roomName("Deluxe Room")
+            .checkIn(LocalTime.of(14, 0))
+            .checkOut(LocalTime.of(11, 0))
+            .bedType("Double Bed")
+            .standardPeople(2)
+            .maxPeople(4)
+            .roomTheme(roomTheme)
+            .build();
+
+        Hotel hotel = Hotel.builder()
+            .hotelName("롯데 시그니엘 호텔")
+            .hotelMainAddress("Seoul")
+            .hotelDetailAddress("123 Yanolja St, Gangnam-gu")
+            .hotelInfoUrl("http://yanoljahotel.com")
+            .room(room)
+            .build();
+
+        entityManager.persist(hotel);
+        return hotel;
+    }
+    private HotelRoomImage createHotelRoomImage(Hotel hotel) {
+        HotelRoomImage hotelRoomImage = HotelRoomImage.builder()
+            .hotel(hotel)
+            .url("http://example.com/hotel-room-image.jpg")
+            .build();
+        entityManager.persist(hotelRoomImage);
+        return hotelRoomImage;
+    }
+
+    private HotelRoomPrice createHotelRoomPrice(Hotel hotel) {
+        HotelRoomPrice hotelRoomPrice = HotelRoomPrice.builder()
+            .hotel(hotel)
+            .peakPrice(200000)
+            .offPeakPrice(150000)
+            .build();
+        entityManager.persist(hotelRoomPrice);
+        return hotelRoomPrice;
+    }
+
+    private YanoljaMember createYanoljaMember() {
+        YanoljaMember yanoljaMember = YanoljaMember.builder()
+            .email("yanolja@example.com")
+            .build();
+        entityManager.persist(yanoljaMember);
+        return yanoljaMember;
+    }
+
+    private Reservation createReservation(Hotel hotel, YanoljaMember yanoljaMember) {
+        Reservation reservation = Reservation.builder()
+            .hotel(hotel)
+            .yanoljaMember(yanoljaMember)
+            .startDate(LocalDate.now())
+            .endDate(LocalDate.now().plusDays(1))
+            .build();
+        entityManager.persist(reservation);
+        return reservation;
+    }
+
+    private Product createProduct(Member member, Reservation reservation) {
+        Product product = Product.builder()
+            .reservation(reservation)
+            .member(member)
+            .bank("하나 은행")
+            .accountNumber("123123")
+            .build();
+        entityManager.persist(product);
+        return product;
+    }
+
+    private PaymentHistory createPaymentHistory(Member member, Product product) {
+        PaymentHistory paymentHistory = PaymentHistory.builder()
+            .member(member)
+            .product(product)
+            .customerEmail("customer@example.com")
+            .customerName("고객 이름")
+            .customerPhoneNumber("010-0000-0000")
+            .price(20000)
+            .paymentType("신용카드")
+            .settlement(false)
+            .build();
+        entityManager.persist(paymentHistory);
+        return paymentHistory;
+    }
+
+
     @Nested
     @DisplayName("구매내역 DB는")
     class Context_purchaseHistory {
@@ -52,90 +162,15 @@ class HistoryRepositoryTest {
         @DisplayName("조회 성공시 구매내역 리스트를 보여준다")
         public void testFindPurchasedHistoriesByMemberId() {
             //given
-            Member member = Member.builder()
-                .email("user@example.com")
-                .password("password")
-                .name("홍길동")
-                .phone("010-1234-5678")
-                .authority(Authority.ROLE_USER)
-                .build();
-            entityManager.persist(member);
-
-            RoomTheme roomTheme = RoomTheme.builder()
-                .parkingZone(true)
-                .breakfast(true)
-                .pool(true)
-                .oceanView(false)
-                .build();
-            entityManager.persist(roomTheme);
-
-            Room room = Room.builder()
-                .roomName("Deluxe Room")
-                .checkIn(LocalTime.of(14, 0))
-                .checkOut(LocalTime.of(11, 0))
-                .bedType("Double Bed")
-                .standardPeople(2)
-                .maxPeople(4)
-                .roomTheme(roomTheme)
-                .build();
-
-
-            Hotel hotel = Hotel.builder()
-                .hotelName("롯데 시그니엘 호텔")
-                .hotelMainAddress("Seoul")
-                .hotelDetailAddress("123 Yanolja St, Gangnam-gu")
-                .hotelInfoUrl("http://yanoljahotel.com")
-                .room(room)
-                .build();
-            entityManager.persist(hotel);
-
-            HotelRoomImage hotelRoomImage = HotelRoomImage.builder()
-                .hotel(hotel) // 이미 생성된 Hotel 인스턴스
-                .url("http://example.com/hotel-room-image.jpg")
-                .build();
-            entityManager.persist(hotelRoomImage);
-
-
-            HotelRoomPrice hotelRoomPrice = HotelRoomPrice.builder()
-                .hotel(hotel)
-                .peakPrice(200000)
-                .offPeakPrice(150000)
-                .build();
-            entityManager.persist(hotelRoomPrice);
-
-            YanoljaMember yanoljaMember = YanoljaMember.builder()
-                .email("yanolja@example.com")
-                .build();
-            entityManager.persist(yanoljaMember);
-
-            Reservation reservation = Reservation.builder()
-                .hotel(hotel)
-                .yanoljaMember(yanoljaMember)
-                .startDate(LocalDate.now())
-                .endDate(LocalDate.now().plusDays(1))
-                .build();
-            entityManager.persist(reservation);
-
-            Product product = Product.builder()
-                .reservation(reservation)
-                .member(member)
-                .bank("하나 은행")
-                .accountNumber("123123")
-                .build();
-            entityManager.persist(product);
-
-            PaymentHistory paymentHistory = PaymentHistory.builder()
-                .member(member)
-                .product(product)
-                .customerEmail("customer@example.com")
-                .customerName("고객 이름")
-                .customerPhoneNumber("010-0000-0000")
-                .price(20000)
-                .paymentType("신용카드")
-                .settlement(false)
-                .build();
-            entityManager.persist(paymentHistory);
-
+            Member member = createMember();
+            RoomTheme roomTheme = createRoomTheme();
+            Hotel hotel = createHotel(roomTheme);
+            createHotelRoomImage(hotel);
+            createHotelRoomPrice(hotel);
+            YanoljaMember yanoljaMember = createYanoljaMember();
+            Reservation reservation = createReservation(hotel, yanoljaMember);
+            Product product = createProduct(member, reservation);
+            PaymentHistory paymentHistory = createPaymentHistory(member, product);
             paymentHistoryRepository.save(paymentHistory);
 
             //when
@@ -147,7 +182,6 @@ class HistoryRepositoryTest {
             assertThat(results).isNotNull();
             assertThat(results.getContent()).isNotNull();
             PurchasedHistoryResponse firstResult = results.getContent().get(0);
-            System.out.println("firstResult = " + firstResult.toString());
             assertThat(firstResult.name()).isEqualTo("롯데 시그니엘 호텔");
             assertThat(firstResult.imageUrl()).isEqualTo("http://example.com/hotel-room-image.jpg");
             assertThat(firstResult.price()).isEqualTo(20000);
@@ -156,110 +190,32 @@ class HistoryRepositoryTest {
     }
 
     @Nested
-    @DisplayName("판매내역 조회쿼리시")
+    @DisplayName("판매내역 조회쿼리 실행시")
     class Context_SaleHistory {
 
         @Test
         @DisplayName("settleMent가 false일시 거래완료라고 표시된다")
         public void testFindPurchasedHistoriesByMemberId() {
             //given
-            Member member = Member.builder()
-                .email("user@example.com")
-                .password("password")
-                .name("홍길동")
-                .phone("010-1234-5678")
-                .authority(Authority.ROLE_USER)
-                .build();
-            entityManager.persist(member);
-
-            RoomTheme roomTheme = RoomTheme.builder()
-                .parkingZone(true)
-                .breakfast(true)
-                .pool(true)
-                .oceanView(false)
-                .build();
-            entityManager.persist(roomTheme);
-
-            Room room = Room.builder()
-                .roomName("Deluxe Room")
-                .checkIn(LocalTime.of(14, 0))
-                .checkOut(LocalTime.of(11, 0))
-                .bedType("Double Bed")
-                .standardPeople(2)
-                .maxPeople(4)
-                .roomTheme(roomTheme)
-                .build();
-
-
-            Hotel hotel = Hotel.builder()
-                .hotelName("롯데 시그니엘 호텔")
-                .hotelMainAddress("Seoul")
-                .hotelDetailAddress("123 Yanolja St, Gangnam-gu")
-                .hotelInfoUrl("http://yanoljahotel.com")
-                .room(room)
-                .build();
-            entityManager.persist(hotel);
-
-            HotelRoomImage hotelRoomImage = HotelRoomImage.builder()
-                .hotel(hotel)
-                .url("http://example.com/hotel-room-image.jpg")
-                .build();
-            entityManager.persist(hotelRoomImage);
-
-
-            HotelRoomPrice hotelRoomPrice = HotelRoomPrice.builder()
-                .hotel(hotel)
-                .peakPrice(200000)
-                .offPeakPrice(150000)
-                .build();
-            entityManager.persist(hotelRoomPrice);
-
-            YanoljaMember yanoljaMember = YanoljaMember.builder()
-                .email("yanolja@example.com")
-                .build();
-            entityManager.persist(yanoljaMember);
-
-            Reservation reservation = Reservation.builder()
-                .hotel(hotel)
-                .yanoljaMember(yanoljaMember)
-                .startDate(LocalDate.now())
-                .endDate(LocalDate.now().plusDays(1))
-                .build();
-            entityManager.persist(reservation);
-
-            Product product = Product.builder()
-                .reservation(reservation)
-                .member(member)
-                .bank("하나 은행")
-                .accountNumber("123123")
-                .build();
-            entityManager.persist(product);
-
-            PaymentHistory paymentHistory = PaymentHistory.builder()
-                .member(member)
-                .product(product)
-                .customerEmail("customer@example.com")
-                .customerName("고객 이름")
-                .customerPhoneNumber("010-0000-0000")
-                .price(20000)
-                .paymentType("신용카드")
-                .settlement(false)
-                .build();
-            entityManager.persist(paymentHistory);
-
-            productRepository.save(product);
+            Member member = createMember();
+            RoomTheme roomTheme = createRoomTheme();
+            Hotel hotel = createHotel(roomTheme);
+            createHotelRoomImage(hotel);
+            createHotelRoomPrice(hotel);
+            YanoljaMember yanoljaMember = createYanoljaMember();
+            Reservation reservation = createReservation(hotel, yanoljaMember);
+            Product product = createProduct(member, reservation);
+            PaymentHistory paymentHistory = createPaymentHistory(member, product);
+            paymentHistoryRepository.save(paymentHistory);
 
             //when
             Page<SaleHistoryResponse> results =
                 productRepository.findSaleHistoriesByMemberId(member.getId(), PageRequest.of(0, 1));
 
-
-
             //then
             assertThat(results).isNotNull();
             assertThat(results.getContent()).isNotNull();
             SaleHistoryResponse firstResult = results.getContent().get(0);
-            System.out.println("firstResult = " + firstResult.toString());
             assertThat(firstResult.saleStatus()).isEqualTo("거래완료");
         }
     }
