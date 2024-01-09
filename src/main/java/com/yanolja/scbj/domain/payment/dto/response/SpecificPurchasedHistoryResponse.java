8b8e5a3abd3106fc956f4377fd0eeb74560ecb22
Yurim Yang <yang980329@likelion.org>
2024-01-10 02:25:05 +0900

    feat: 구매내역 조회 비즈니스 로직 구현
    - 구매내역 상세 조회를 위한 Controller, Service, DTO 완성
    - 예외처리 추가

---
+++ src/main/java/com/yanolja/scbj/domain/payment/dto/response/SpecificPurchasedHistoryResponse.java
@@ -0,0 +1,24 @@
+package com.yanolja.scbj.domain.payment.dto.response;
+
+import lombok.Builder;
+
+public record SpecificPurchasedHistoryResponse(
+    String hotelName,
+    String roomName,
+    int standardPeople,
+    int maxPeople,
+    String checkIn,
+    String checkOut,
+    String customerName,
+    String customerPhoneNumber,
+    long paymentHistoryId,
+    String paymentType,
+    int originalPrice,
+    int price,
+    int remainingDays
+) {
+
+    @Builder
+    public SpecificPurchasedHistoryResponse {
+    }
+}
