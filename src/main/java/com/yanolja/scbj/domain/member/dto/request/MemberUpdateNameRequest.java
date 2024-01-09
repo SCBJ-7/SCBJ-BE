6ba32d67b2332973377ade6e0fac67d4003960b6
jaecheol sim <wocjf0513@naver.com>
2024-01-09 14:00:38 +0900

    fix, test: MemberDTO가 생성됨에 따라 그에 맞게 TEST 수정

--- src/main/java/com/yanolja/scbj/domain/member/dto/request/MemberUpdateNameRequest.java
+++ src/main/java/com/yanolja/scbj/domain/member/dto/request/MemberUpdateNameRequest.java
@@ -6,6 +6,7 @@ import com.yanolja.scbj.domain.member.validation.ValidationGroups.SizeGroup;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 
 public record MemberUpdateNameRequest(
     @NotBlank(message = "이름의 길이는 1~20자여야 합니다.", groups = NotBlankGroup.class)
@@ -15,6 +16,7 @@ public record MemberUpdateNameRequest(
 
 ) {
 
+    @Builder
     public MemberUpdateNameRequest(String name) {
         this.name = name;
     }
