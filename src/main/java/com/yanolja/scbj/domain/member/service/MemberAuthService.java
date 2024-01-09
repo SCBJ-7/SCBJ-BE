6fbb52e06190c576a77038ab98226d548b78ba07
jaecheol sim <wocjf0513@naver.com>
2024-01-09 16:43:14 +0900

    fix: 토큰 재발급시, 액세스 토큰 만료 여부 예외에 상관없이 재발급 가능하게

--- src/main/java/com/yanolja/scbj/domain/member/service/MemberAuthService.java
+++ src/main/java/com/yanolja/scbj/domain/member/service/MemberAuthService.java
@@ -6,6 +6,7 @@ import com.yanolja.scbj.domain.member.exception.InvalidRefreshTokenException;
 import com.yanolja.scbj.global.config.CustomUserDetailsService;
 import com.yanolja.scbj.global.config.jwt.JwtUtil;
 import com.yanolja.scbj.global.exception.ErrorCode;
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -21,19 +22,23 @@ public class MemberAuthService {
     }
 
     public TokenResponse refreshAccessToken(final RefreshRequest refreshRequest) {
+        String username = null;
+        try {
+            username = jwtUtil.extractUsername(refreshRequest.getAccessToken().substring(7));
+        } catch (ExpiredJwtException e) {
+            username = e.getClaims().getSubject();
+        } finally {
+            if (jwtUtil.isRefreshTokenValid(username, refreshRequest.getRefreshToken())) {
+                UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+                String newAccessToken = jwtUtil.generateToken(userDetails);
+                String newRefreshToken = jwtUtil.generateRefreshToken(username);
 
-        String username = jwtUtil.extractUsername(refreshRequest.getAccessToken().substring(7));
-        if (jwtUtil.isRefreshTokenValid(username, refreshRequest.getRefreshToken())) {
-            UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
-            String newAccessToken = jwtUtil.generateToken(userDetails);
-            String newRefreshToken = jwtUtil.generateRefreshToken(username);
-
-            return TokenResponse.builder().accessToken(newAccessToken).refreshToken(newRefreshToken)
-                .build();
-
-        } else {
-            throw new InvalidRefreshTokenException(ErrorCode.INVALID_REFRESH_TOKEN);
+                return TokenResponse.builder().accessToken(newAccessToken)
+                    .refreshToken(newRefreshToken)
+                    .build();
+            } else {
+                throw new InvalidRefreshTokenException(ErrorCode.INVALID_REFRESH_TOKEN);
+            }
         }
     }
-
 }
