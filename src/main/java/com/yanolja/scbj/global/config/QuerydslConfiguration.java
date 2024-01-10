6f397c6a04800e53b1b2e5958fcb4d2579067bf6
Aleexender <alexkjh2011@gmail.com>
2024-01-11 04:42:59 +0900

    fix: EnableAudititing 어노테이션 제거
    해당 EnableAudititing이 여러개면 중복으로 인한 에러가 발생하기 때문에 지웠습니다

--- src/main/java/com/yanolja/scbj/global/config/QuerydslConfiguration.java
+++ src/main/java/com/yanolja/scbj/global/config/QuerydslConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @Configuration
 public class QuerydslConfiguration {
 
