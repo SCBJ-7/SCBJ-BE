67a2ad0f7afd40db4c35417c275100cffe5ab8d5
kwonMInwoo <qwerty029369@naver.com>
2024-01-11 16:46:10 +0900

    feat: response 생성
    - 카카오 페이 연동에 필요한 response 생성

---
+++ src/main/java/com/yanolja/scbj/domain/payment/dto/response/PaymentReadyResponse.java
@@ -0,0 +1,20 @@
+package com.yanolja.scbj.domain.payment.dto.response;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@Getter
+public class PaymentReadyResponse{
+    private String tid;
+    private String next_redirect_pc_url;
+    private String next_redirect_mobile_url;
+    private String next_redirect_app_url;
+    private String android_app_scheme;
+    private String ios_app_scheme;
+    private String created_at;
+//    String androidAppScheme,
+//    String iosAppScheme,
+//    LocalDateTime createdAt
+
+}
