064d5cb49782a51b6f4d6673b902e6457934ee07
kwonMInwoo <qwerty029369@naver.com>
2024-01-11 14:39:10 +0900

    feat: 크롤링 로직 작성 완료
    - 야놀자 사이트에서 호텔 정보를 크롤링하는 코드작성 완료

--- src/main/java/com/yanolja/scbj/domain/crawling/CrawlingYanolja.java
+++ src/main/java/com/yanolja/scbj/domain/crawling/CrawlingYanolja.java
@@ -1,15 +1,17 @@
 package com.yanolja.scbj.domain.crawling;
 
 import com.yanolja.scbj.domain.hotelRoom.entity.Hotel;
+import com.yanolja.scbj.domain.hotelRoom.entity.HotelRoomImage;
 import com.yanolja.scbj.domain.hotelRoom.entity.HotelRoomPrice;
 import com.yanolja.scbj.domain.hotelRoom.entity.Room;
 import com.yanolja.scbj.domain.hotelRoom.entity.RoomTheme;
+import com.yanolja.scbj.domain.hotelRoom.repository.HotelRoomImageRepository;
 import com.yanolja.scbj.domain.hotelRoom.repository.HotelRoomPriceRepository;
 import com.yanolja.scbj.domain.hotelRoom.repository.HotelRoomRepository;
-import com.yanolja.scbj.domain.hotelRoom.repository.RoomThemeRepository;
 import java.time.Duration;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -23,128 +25,139 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Transactional
 public class CrawlingYanolja {
-    private final String baseUrl = "https://www.yanolja.com/hotel/r-";
+
+    private final String BASE_URL = "https://www.yanolja.com/hotel/r-";
+    private final String SUFFIX_URL = "?advert=AREA&topAdvertiseMore=0";
+    private final String CLASS_NAME = "class name";
+    private final String CSS_SELECTOR = "css selector";
+    private final String XPATH = "xpath";
     private final HotelRoomRepository hotelRoomRepository;
     private final HotelRoomPriceRepository hotelRoomPriceRepository;
-
-    // 강남/역삼/삼성, 신사/청담/압구정, 해운대/마린시티, 벡스코/센텀시티, 제주시/제주국제공항, 서귀포시/모슬포, 가평/청평/양평, 수원/화성,
-    // 송도/소래포구, 인천국제공항/강화/을왕리, 강릉, 속초/고성, 대구/구미/안동/문경, 경주, 전주/완주, 광주/나주/함평, 대전/세종, 천안/아산/도고
-    // , "910162" 서귀포시/모슬포 빠짐
-    private String[] urlArray = {
-        "910062", "910063", "910078", "910079", "910088", "910091", "910167", "910101",
-        "910102", "910108", "910279", "910112", "910113", "910258", "910120", "910125", "910126"
+    private final HotelRoomImageRepository hotelRoomImageRepository;
+    private final String[] localNumberArray = {
+        "900582", "900583", "900584", "900585", "900586", "900587", "900588", "900589", "900590"
     };
-
-    private String[] localNameArray = {
-        "서울", "서울", "부산", "부산", "제주", "경기", "경기", "인천", "인천", "강원", "강원", "경상",
-        "경상", "전라", "전라", "충청", "충청"
+    private final String[] localNameArray = {
+        "서울", "부산", "제주", "경기", "인천", "강원", "경상", "전라", "충청"
     };
-
-    private String[] checkInArray = {
-        "15:00", "14:00", "13:00"
-    };
-
-    private String[] checkOutArray = {
-        "10:00", "11:00", "12:00"
-    };
-
-    private String[] bedTypeArray = {
+    private final String[] bedTypeArray = {
         "싱글 침대", "더블 침대", "퀸 침대", "킹 침대",
     };
 
-    private final String suffix = "?advert=AREA&topAdvertiseMore=0";
-
     @GetMapping("/crawling")
-    public void crawling(){
-        String hotelNameClassName = "css-1g3ik0v";
-        String roomNameClassName = "css-1rr4h0w";
-        String peopleNumClassName = "css-18j6obq";
-        String priceClassName = "css-g269i3";
-        String placeClassName = "css-11ynnk0";
-
-        String hotelImg = "css-sr2c7j";
-
+    public void crawling() {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--no-sandbox");
-        chromeOptions.addArguments("--start-minimized");
         chromeOptions.addArguments("--disable--gpu");
+        chromeOptions.addArguments("--start-minimized");
         chromeOptions.addArguments("--disable-popup-blocking");
-        System.setProperty("webdriver.chrome.driver", "/Users/qwert/Downloads/chromedriver-mac-arm64/chromedriver");
+        System.setProperty("webdriver.chrome.driver",
+            "/Users/qwert/Downloads/chromedriver-mac-arm64/chromedriver");
         ChromeDriver chromeDriver = new ChromeDriver(chromeOptions);
 
+        for (int i = 0; i < localNumberArray.length; i++) {
+            String localNumber = localNumberArray[i];
+            String url = BASE_URL + localNumber + SUFFIX_URL;
 
-        for (int i = 0; i < urlArray.length; i++) {
-            String local = urlArray[i];
-            String url = baseUrl + local + suffix;
             chromeDriver.get(url);
-            chromeDriver.manage().timeouts().implicitlyWait(Duration.ofMillis(4000));
-            try {
-                List<WebElement> webElementsList = chromeDriver.findElements(
-                    By.cssSelector("div.PlaceListBody_listGroup__LddQf > div > div > a"));
-                for (WebElement a : webElementsList) {
+            chromeDriver.manage().timeouts().implicitlyWait(Duration.ofMillis(3000));
 
-                    String hotelUrl = a.getAttribute("href");
+            String hotelDetailUrlPath = "div.PlaceListBody_listGroup__LddQf > div > div > a";
+            List<WebElement> hotelDetailElements = chromeDriver.findElements(
+                By.cssSelector(hotelDetailUrlPath));
+
+            System.err.println(hotelDetailElements.size());
+
+            for (WebElement hotelDetailElement : hotelDetailElements) {
+                try {
+                    String hotelUrl = hotelDetailElement.getAttribute("href");
                     System.out.println(hotelUrl);
-                    ChromeDriver secondChromeDriver = new ChromeDriver(chromeOptions);
+
+                    ChromeDriver secondChromeDriver = new ChromeDriver();
                     secondChromeDriver.get(hotelUrl);
+
                     secondChromeDriver.manage().timeouts()
-                        .implicitlyWait(Duration.ofMillis(4000));
+                        .implicitlyWait(Duration.ofMillis(3000));
 
-                    WebElement hotelNameElement = secondChromeDriver.findElement(
-                        By.className(hotelNameClassName));
+                    // 호텔 이름
+                    String hotelNameClassName = "css-1g3ik0v";
+                    WebElement hotelNameElement = getElement(CLASS_NAME, hotelNameClassName,
+                        secondChromeDriver);
 
-                    WebElement roomNameElement = secondChromeDriver.findElement(
-                        By.cssSelector("h3.css-deizzc > div." + roomNameClassName));
+                    // 방 이름
+                    String roomNameCssSelector = "h3.css-deizzc > div.css-1rr4h0w";
+                    WebElement roomNameElement = getElement(CSS_SELECTOR, roomNameCssSelector,
+                        secondChromeDriver);
 
-                    WebElement timeElement = secondChromeDriver.findElement(
-                        By.cssSelector("a.css-1w7jlh2 > div.css-1qxtkjb"));
+                    // 체크인, 체크아웃
+                    String timeCssSelector = "a.css-1w7jlh2 > div.css-1qxtkjb";
+                    WebElement timeElement = getElement(CSS_SELECTOR, timeCssSelector,
+                        secondChromeDriver);
 
-                                        String[] timeArray = timeElement.getText().split("\n");
+                    String[] timeArray = timeElement.getText().split("\n");
                     int checkInHour = Integer.parseInt(timeArray[1].split(":")[0]);
                     int checkInMin = Integer.parseInt(timeArray[1].split(":")[1]);
 
                     int checkOutHour = Integer.parseInt(timeArray[3].split(":")[0]);
                     int checkOutMin = Integer.parseInt(timeArray[3].split(":")[1]);
 
-                    WebElement peopleNumElement = secondChromeDriver.findElement(
-                        By.className(peopleNumClassName));
+                    // 기준인원, 최대인원
+                    String peopleNumClassName = "css-18j6obq";
+                    WebElement peopleNumElement = getElement(CLASS_NAME, peopleNumClassName,
+                        secondChromeDriver);
 
-                    WebElement hotelImgElement = secondChromeDriver.findElement(
-                        By.cssSelector("div.swiper-wrapper > div.swiper-slide swiper-slide-visible swiper-slide-active > div > span > img"));
+                    char[] peopleNumArray = peopleNumElement.getText().toCharArray();
 
-                    System.out.println(hotelImgElement.getAttribute("src"));
+                    // 호텔 이미지, 방 이미지
+                    String imgPath = "//*[@id=\"__next\"]/div/div/main/article/div[1]/section/div[1]/div/div/div[3]/div/span/img";
+                    WebElement hotelImgElement = getElement(XPATH, imgPath, secondChromeDriver);
 
-//                    WebElement priceElement = null;
-//                    boolean hasPriceElement = secondChromeDriver.findElement(By.xpath(
-//                            "section.css-1qwzivr > div > a > div.css-g269i3 > div.rack_price"))
-//                        .isDisplayed();
-//
-//                    if(hasPriceElement){
-//                        priceElement = secondChromeDriver.findElement(
-//                            By.cssSelector("section.css-1qwzivr > div > a > div.css-g269i3 > div.rack_price"));
-//                    }else if{
-//                        priceElement = secondChromeDriver.findElement(
-//                            By.cssSelector("section.css-1qwzivr > div > a > div.css-g269i3 > div.price "));
-//                    }
+                    String hotelImgUrl = hotelImgElement.getAttribute("src");
 
-//                    price  랜덤이 나을듯? ?
+                    String nextImgBtnClassName = "css-ln49wb";
+                    WebElement nextBtn = getElement(CLASS_NAME, nextImgBtnClassName,
+                        secondChromeDriver);
+                    nextBtn.click();
 
-//                    System.out.println(priceElement.getText());
-                    WebElement priceElement = null;
-                    WebElement addressElement = secondChromeDriver.findElement(
-                        By.cssSelector("div." + placeClassName + " > div.css-cxbger > div > span"));
+                    WebElement roomImgElement = getElement(XPATH, imgPath, secondChromeDriver);
+                    String roomImgUrl = roomImgElement.getAttribute("src");
+
+                    // 호텔 주소
+                    String hotelAddressSelector = "div.css-11ynnk0 > div.css-cxbger > div> span";
+                    WebElement addressElement = getElement(CSS_SELECTOR, hotelAddressSelector,
+                        secondChromeDriver);
+
+                    // 가격
+                    Random random = new Random();
+                    int min = 20;
+                    int max = 50;
+                    int randomNum = random.nextInt(max - min + 1) + min;
+                    int price = randomNum * 10000;
+
+                    // 룸 테마
+                    boolean hasParkingZone = random.nextBoolean();
+                    boolean hasPool = random.nextBoolean();
+                    boolean hasOceanView = random.nextBoolean();
+                    boolean hasBreakfast = random.nextBoolean();
+
+                    // 베드 타입
+                    int randomBedTypeNum = random.nextInt(4);
+                    String randomBedType = bedTypeArray[randomBedTypeNum];
 
                     RoomTheme roomTheme = RoomTheme.builder()
-                        .parkingZone(true)
+                        .parkingZone(hasParkingZone)
+                        .pool(hasPool)
+                        .oceanView(hasOceanView)
+                        .breakfast(hasBreakfast)
                         .build();
 
                     Room room = Room.builder()
                         .roomName(roomNameElement.getText())
                         .checkIn(LocalTime.of(checkInHour, checkInMin))
                         .checkOut(LocalTime.of(checkOutHour, checkOutMin))
-                        .standardPeople(peopleNumElement.getText().indexOf(3))
-                        .maxPeople(peopleNumElement.getText().indexOf(11))
-                        .bedType("싱글 침대")
+                        .standardPeople(Integer.parseInt(String.valueOf(peopleNumArray[3])))
+                        .maxPeople(Integer.parseInt(String.valueOf(peopleNumArray[11])))
+                        .bedType(randomBedType)
                         .roomTheme(roomTheme)
                         .build();
 
@@ -156,21 +169,48 @@ public class CrawlingYanolja {
                         .hotelInfoUrl(hotelUrl)
                         .build();
 
-                    HotelRoomPrice hotelRoomPrice = HotelRoomPrice.builder()
+                    HotelRoomImage hotelImage = HotelRoomImage.builder()
                         .hotel(hotel)
-//                        .peakPrice(Integer.parseInt(priceElement.getText().replace(",", "")))
-//                        .offPeakPrice((int) (Integer.parseInt(priceElement.getText().replace(",", "")) * 0.8))
+                        .url(hotelImgUrl)
                         .build();
 
-//                    hotelRoomRepository.save(hotel);
-//                    hotelRoomPriceRepository.save(hotelRoomPrice);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                System.err.println("error");
-            }
+                    HotelRoomImage roomImage = HotelRoomImage.builder()
+                        .hotel(hotel)
+                        .url(roomImgUrl)
+                        .build();
 
+                    HotelRoomPrice hotelRoomPrice = HotelRoomPrice.builder()
+                        .hotel(hotel)
+                        .peakPrice(price)
+                        .offPeakPrice((int) (price * 0.8))
+                        .build();
+
+                    hotelRoomRepository.save(hotel);
+                    hotelRoomPriceRepository.save(hotelRoomPrice);
+                    hotelRoomImageRepository.save(hotelImage);
+                    hotelRoomImageRepository.save(roomImage);
+
+                    secondChromeDriver.quit();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.err.println("error");
+                }
+            }
         }
     }
+
+    private WebElement getElement(String type, String selector, ChromeDriver driver) {
+        switch (type) {
+            case "class name":
+                return driver.findElement(By.className(selector));
+            case "css selector":
+                return driver.findElement(By.cssSelector(selector));
+            case "xpath":
+                return driver.findElement(By.xpath(selector));
+            default:
+                return null;
+        }
+    }
+
 }
 
