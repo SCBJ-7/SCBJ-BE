4c6b3f286bf159e5615bbdb6dc7e0c3a0eb11870
kwonMInwoo <qwerty029369@naver.com>
2024-01-12 14:53:49 +0900

    test: 결제 페이지 조회 테스트 코드
    - 결제 페이지 조회 로직의 컨트롤러, 서비스 테스트 코드 작성 완료

---
+++ src/test/java/com/yanolja/scbj/domain/payment/controller/PaymentRestControllerTest.java
@@ -0,0 +1,75 @@
+package com.yanolja.scbj.domain.payment.controller;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.yanolja.scbj.domain.payment.dto.response.PaymentPageFindResponse;
+import com.yanolja.scbj.domain.payment.service.PaymentService;
+import com.yanolja.scbj.domain.product.controller.ProductRestController;
+import com.yanolja.scbj.domain.product.service.ProductService;
+import com.yanolja.scbj.global.config.SecurityConfig;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+@WebMvcTest(
+    controllers = PaymentRestController.class,
+    excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class),
+    },
+    excludeAutoConfiguration = SecurityAutoConfiguration.class
+)
+class PaymentRestControllerTest {
+    @Autowired
+    private MockMvc mvc;
+    @MockBean
+    private PaymentService paymentService;
+
+    @Nested
+    @DisplayName("결제 페이지 조회는 ")
+    class Context_PaymentPageFind {
+
+        @Test
+        @DisplayName("성공 시 결제할 상품의 정보를 반환한다.")
+        void _will_success() throws Exception{
+            // given
+            Long targetProductId = 1L;
+
+            PaymentPageFindResponse findResponse = PaymentPageFindResponse.builder()
+                .hotelName("양도 호텔")
+                .hotelImage("asdasfnaijsndijkv.jpg")
+                .roomName("룸1")
+                .standardPeople(2)
+                .maxPeople(4)
+                .checkInDateTime(LocalDateTime.now())
+                .checkOutDateTime(LocalDateTime.now())
+                .originalPrice(200000)
+                .salePrice(100000)
+                .build();
+
+            given(paymentService.getPaymentPage(targetProductId)).willReturn(findResponse);
+
+            // when
+            ResultActions result = mvc.perform(get("/v1/products/" + targetProductId + "/payments"));
+
+            // then
+            result.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.data.hotelName", is("양도 호텔")));
+        }
+    }
+}
---
+++ src/test/java/com/yanolja/scbj/domain/payment/service/PaymentServiceTest.java
@@ -0,0 +1,118 @@
+package com.yanolja.scbj.domain.payment.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+
+import com.yanolja.scbj.domain.hotelRoom.entity.Hotel;
+import com.yanolja.scbj.domain.hotelRoom.entity.HotelRoomImage;
+import com.yanolja.scbj.domain.hotelRoom.entity.HotelRoomPrice;
+import com.yanolja.scbj.domain.hotelRoom.entity.Room;
+import com.yanolja.scbj.domain.hotelRoom.entity.RoomTheme;
+import com.yanolja.scbj.domain.payment.dto.response.PaymentPageFindResponse;
+import com.yanolja.scbj.domain.payment.entity.PaymentHistory;
+import com.yanolja.scbj.domain.payment.repository.PaymentHistoryRepository;
+import com.yanolja.scbj.domain.product.entity.Product;
+import com.yanolja.scbj.domain.product.repository.ProductRepository;
+import com.yanolja.scbj.domain.product.service.ProductDtoConverter;
+import com.yanolja.scbj.domain.reservation.entity.Reservation;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+class PaymentServiceTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private PaymentService paymentService;
+
+    @Nested
+    @DisplayName("결제 페이지 조회는 ")
+    class Context_PaymentPageFind {
+
+        @Test
+        @DisplayName("성공 시 결제 할 상품의 정보를 반환한다.")
+        void _will_success() {
+            // given
+            Long targetProductId = 1L;
+            RoomTheme roomTheme = RoomTheme.builder()
+                .id(1L)
+                .build();
+
+            Room room = Room.builder()
+                .checkIn(LocalTime.now())
+                .checkOut(LocalTime.now())
+                .roomTheme(roomTheme)
+                .build();
+
+            HotelRoomPrice hotelRoomPrice = HotelRoomPrice.builder()
+                .id(1L)
+                .offPeakPrice(100000)
+                .peakPrice(200000)
+                .build();
+
+            HotelRoomImage hotelRoomImage = HotelRoomImage.builder()
+                .url("asdasdasdasd.jpg")
+                .build();
+
+            Hotel hotel = Hotel.builder()
+                .id(1L)
+                .room(room)
+                .hotelRoomImageList(List.of(hotelRoomImage))
+                .hotelRoomPrice(hotelRoomPrice)
+                .build();
+
+            Reservation reservation = Reservation.builder()
+                .id(1L)
+                .hotel(hotel)
+                .startDate(LocalDate.now())
+                .endDate(LocalDate.now())
+                .build();
+
+            PaymentHistory paymentHistory = PaymentHistory.builder()
+                .id(1L)
+                .price(100000)
+                .customerName("tester")
+                .customerEmail("qwe@nav.com")
+                .customerPhoneNumber("010-1122-3344")
+                .paymentType("카카오페이")
+                .settlement(true)
+                .build();
+
+            Product product = Product.builder()
+                .id(1L)
+                .firstPrice(200000)
+                .secondPrice(100000)
+                .bank("국민")
+                .accountNumber("12512-2131-12512")
+                .secondGrantPeriod(24)
+                .reservation(reservation)
+                .paymentHistory(paymentHistory)
+                .build();
+
+            given(productRepository.findProductById(targetProductId)).willReturn(Optional.of(product));
+
+            // when
+            PaymentPageFindResponse response = paymentService.getPaymentPage(targetProductId);
+
+            // then
+            Assertions.assertThat(response).isNotNull();
+            Assertions.assertThat(response.hotelName()).isEqualTo(hotel.getHotelName());
+        }
+    }
+
+
+
+}
