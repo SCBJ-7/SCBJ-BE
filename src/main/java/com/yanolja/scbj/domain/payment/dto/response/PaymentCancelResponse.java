50c5c833b4f66d0dadbe3e46a2758a591355c0cd
kwonMInwoo <qwerty029369@naver.com>
2024-01-12 17:21:45 +0900

    feat: response 추가
    - 취소에서 필요한 response 추가

---
+++ src/main/java/com/yanolja/scbj/domain/payment/dto/response/PaymentCancelResponse.java
@@ -0,0 +1,10 @@
+package com.yanolja.scbj.domain.payment.dto.response;
+
+import lombok.Data;
+
+@Data
+public class PaymentCancelResponse {
+    private String item_name;
+//    private PaymentAmountResponse amount;
+
+}
