db389909758cf07662606c1d3d7abe3f414fec78
kwonMInwoo <qwerty029369@naver.com>
2024-01-12 14:26:32 +0900

    refactor: 카카오페이 api 로직 리팩토링
    - 메서드 분할 및 네이밍 수정

--- src/main/java/com/yanolja/scbj/domain/payment/controller/PaymentRestController.java
+++ src/main/java/com/yanolja/scbj/domain/payment/controller/PaymentRestController.java
@@ -1,16 +1,20 @@
 package com.yanolja.scbj.domain.payment.controller;
 
+import com.yanolja.scbj.domain.payment.dto.request.PaymentReadyRequest;
 import com.yanolja.scbj.domain.payment.dto.response.PaymentApproveResponse;
 import com.yanolja.scbj.domain.payment.service.PaymentService;
 import com.yanolja.scbj.global.common.ResponseDTO;
 import com.yanolja.scbj.global.util.SecurityUtil;
+import jakarta.validation.Valid;
 import java.util.List;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -27,21 +31,20 @@ public class PaymentRestController {
 
     @PostMapping("/v1/products/{product_id}/payments")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseDTO<Object> reqeustPayments(@PathVariable("product_id") long productId) {
-        String url = paymentService.kakaoPayReady(securityUtil.getCurrentMemberId(), productId);
+    public ResponseDTO<Void> reqeustPayments(@PathVariable("product_id") long productId,
+    @Valid @RequestBody PaymentReadyRequest paymentReadyRequest) {
+        String url = paymentService.kakaoPayReady(securityUtil.getCurrentMemberId(), productId, paymentReadyRequest);
         System.out.println(url);
         return ResponseDTO.res("결제에 성공했습니다.");
     }
 
     @GetMapping("/kakaopay-success")
     @ResponseStatus(HttpStatus.OK)
-    public PaymentApproveResponse good(@RequestParam("pg_token") String pgToken,
-        @RequestParam("memberId") Long memberId, @RequestParam("orderId") Long orderId) {
-        System.out.println(memberId);
-        System.out.println(orderId);
+    public ResponseDTO<PaymentApproveResponse> good(@RequestParam("pg_token") String pgToken,
+        @RequestParam("memberId") Long memberId) {
         PaymentApproveResponse paymentApproveResponse = paymentService.KakaoPayInfo(pgToken,
-            memberId, orderId);
-        return paymentApproveResponse;
+            memberId);
+        return ResponseDTO.res("결제에 성공했습니다.");
     }
 
 }
--- src/main/java/com/yanolja/scbj/domain/payment/service/PaymentService.java
+++ src/main/java/com/yanolja/scbj/domain/payment/service/PaymentService.java
@@ -1,9 +1,13 @@
 package com.yanolja.scbj.domain.payment.service;
 
 import com.yanolja.scbj.domain.hotelRoom.entity.Hotel;
+import com.yanolja.scbj.domain.member.entity.Member;
+import com.yanolja.scbj.domain.member.exception.MemberNotFoundException;
 import com.yanolja.scbj.domain.member.repository.MemberRepository;
+import com.yanolja.scbj.domain.payment.dto.request.PaymentReadyRequest;
 import com.yanolja.scbj.domain.payment.dto.response.PaymentApproveResponse;
 import com.yanolja.scbj.domain.payment.dto.response.PaymentReadyResponse;
+import com.yanolja.scbj.domain.payment.entity.PaymentAgreement;
 import com.yanolja.scbj.domain.payment.entity.PaymentHistory;
 import com.yanolja.scbj.domain.payment.repository.PaymentHistoryRepository;
 import com.yanolja.scbj.domain.product.entity.Product;
@@ -12,20 +16,27 @@ import com.yanolja.scbj.domain.product.exception.ProductNotFoundException;
 import com.yanolja.scbj.domain.product.repository.ProductRepository;
 import com.yanolja.scbj.domain.reservation.entity.Reservation;
 import com.yanolja.scbj.global.exception.ErrorCode;
-import com.yanolja.scbj.global.util.SecurityUtil;
 import jakarta.annotation.PostConstruct;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
@@ -34,15 +45,19 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class PaymentService {
 
+    private final String KEY_PREFIX = "kakaoPay";
+    private final String PAY_TYPE = "카카오페이";
+
     private final ProductRepository productRepository;
-    private PaymentReadyResponse paymentReadyResponse;
     private final MemberRepository memberRepository;
-    private PaymentApproveResponse paymentApproveResponse;
     private final PaymentHistoryRepository paymentHistoryRepository;
+    private final RedisTemplate<String, String> redisTemplate;
 
     private HttpHeaders headers;
+
     @Value("${kakao-api.base-url}")
     private String baseUrl;
+
     @Value("${kakao-api.api-key}")
     private String key;
 
@@ -54,7 +69,8 @@ public class PaymentService {
         headers.add("Content-Type", MediaType.APPLICATION_FORM_URLENCODED_VALUE + ";charset=UTF-8");
     }
 
-    public String kakaoPayReady(Long memberId, Long productId){
+    public String kakaoPayReady(Long memberId, Long productId,
+        PaymentReadyRequest paymentReadyRequest) {
 
         Product targetProduct = productRepository.findById(productId)
             .orElseThrow(() -> new ProductNotFoundException(
@@ -71,8 +87,9 @@ public class PaymentService {
         LocalDateTime checkInDateTime = LocalDateTime.of(targetReservation.getStartDate(),
             targetHotel.getRoom().getCheckIn());
 
-        int price = 0;
-        if(targetProduct.getSecondGrantPeriod() != SecondTransferExistence.NOT_EXISTS.getStatus()){
+        int price = targetProduct.getFirstPrice();
+        if (targetProduct.getSecondGrantPeriod()
+            != SecondTransferExistence.NOT_EXISTS.getStatus()) {
             int secondGrantPeriod = targetProduct.getSecondGrantPeriod();
             changeTime = checkInDateTime.minusHours(secondGrantPeriod);
 
@@ -89,16 +106,28 @@ public class PaymentService {
         params.add("quantity", 1);
         params.add("total_amount", price);
         params.add("tax_free_amount", 0);
-        params.add("approval_url", "http://localhost:8080/kakaopay-success?memberId=" + memberId + "&orderId=" + productId);
+        params.add("approval_url", "http://localhost:8080/kakaopay-success?memberId=" + memberId);
         params.add("cancel_url", "http://localhost:8080/kakaoPayCancel");
         params.add("fail_url", "http://localhost:8080/kakaoPaySuccessFail");
 
-        // redis > (memberId + tid)
-
         HttpEntity<MultiValueMap<String, Object>> body = new HttpEntity<>(params, headers);
         try {
-            paymentReadyResponse = restTemplate.postForObject(new URI("https://kapi.kakao.com/v1/payment/ready"), body,
+            PaymentReadyResponse paymentReadyResponse = restTemplate.postForObject(
+                new URI("https://kapi.kakao.com/v1/payment/ready"), body,
                 PaymentReadyResponse.class);
+
+            Map<String, String> redisMap = new HashMap<>();
+            redisMap.put("productId", String.valueOf(productId));
+            redisMap.put("tid", paymentReadyResponse.getTid());
+            redisMap.put("price", String.valueOf(price));
+            redisMap.put("customerName", paymentReadyRequest.customerName());
+            redisMap.put("customerEmail", paymentReadyRequest.customerEmail());
+            redisMap.put("customerPhoneNumber", paymentReadyRequest.customerPhoneNumber());
+
+            HashOperations<String, String, String> hashOperations = redisTemplate.opsForHash();
+            String key = KEY_PREFIX + memberId;
+            hashOperations.putAll(key, redisMap);
+
             return paymentReadyResponse.getNext_redirect_pc_url();
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
@@ -107,32 +136,55 @@ public class PaymentService {
     }
 
     // 결제 승인 요청
-    public PaymentApproveResponse KakaoPayInfo( String pgToken, Long memberId, Long orderId){
+    @Transactional
+    public PaymentApproveResponse KakaoPayInfo(String pgToken, Long memberId){
 
         RestTemplate restTemplate = new RestTemplate();
 
-        // tid 조회 필요
+        String key = KEY_PREFIX + memberId;
+        String productId = (String) redisTemplate.opsForHash().get(key,"productId");
+        String customerName = (String) redisTemplate.opsForHash().get(key, "customerName");
+        String customerEmail = (String) redisTemplate.opsForHash().get(key, "customerEmail");
+        String customerPhoneNumber = (String) redisTemplate.opsForHash().get(key, "customerPhoneNumber");
+        String price = (String) redisTemplate.opsForHash().get(key, "price");
+        String tid = (String) redisTemplate.opsForHash().get(key, "tid");
 
         MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
         params.add("cid", "TC0ONETIME");
-        params.add("tid", paymentReadyResponse.getTid());
-        params.add("partner_order_id", String.valueOf(orderId));
+        params.add("tid", tid);
+        params.add("partner_order_id", String.valueOf(productId));
         params.add("partner_user_id", String.valueOf(memberId));
         params.add("pg_token", pgToken);
 
         HttpEntity<MultiValueMap<String, Object>> body = new HttpEntity<>(params, headers);
 
         try {
-            paymentApproveResponse = restTemplate.postForObject(new URI(baseUrl + "/approve"), body,
+            PaymentApproveResponse paymentApproveResponse = restTemplate.postForObject(new URI(baseUrl + "/approve"), body,
                 PaymentApproveResponse.class);
 
-            System.out.println(paymentApproveResponse.getAmount().getTotal());
-//            PaymentHistory.builder()
-//                    .member()
-//                        .product()
-//            paymentHistoryRepository.save()
-            return paymentApproveResponse;
+            Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberNotFoundException(ErrorCode.MEMBER_NOT_FOUND));
 
+            Product product = productRepository.findById(Long.valueOf(productId))
+                .orElseThrow(() -> new ProductNotFoundException(ErrorCode.PRODUCT_NOT_FOUND));
+
+            PaymentAgreement agreement = PaymentAgreement.builder()
+                .build();
+
+            PaymentHistory paymentHistory = PaymentHistory.builder()
+                .member(member)
+                .product(product)
+                .customerName(customerName)
+                .customerEmail(customerEmail)
+                .customerPhoneNumber(customerPhoneNumber)
+                .paymentAgreement(agreement)
+                .price(Integer.parseInt(price))
+                .paymentType(PAY_TYPE)
+                .build();
+
+            paymentHistoryRepository.save(paymentHistory);
+
+            return paymentApproveResponse;
         } catch (RestClientException e) {
             e.printStackTrace();
         } catch (URISyntaxException e) {
