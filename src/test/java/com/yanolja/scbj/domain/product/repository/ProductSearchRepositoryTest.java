7d2c4ba568f9a93324754f81303d2ce327fe259a
kwonMInwoo <qwerty029369@naver.com>
2024-01-17 16:41:34 +0900

    test: 배타락 테스트 코드 설정
    - 배타락을 테스트하는 코드 작성

--- src/test/java/com/yanolja/scbj/domain/product/repository/ProductSearchRepositoryTest.java
+++ src/test/java/com/yanolja/scbj/domain/product/repository/ProductSearchRepositoryTest.java
@@ -6,43 +6,81 @@ import com.yanolja.scbj.domain.hotelRoom.entity.Hotel;
 import com.yanolja.scbj.domain.hotelRoom.entity.HotelRoomImage;
 import com.yanolja.scbj.domain.hotelRoom.entity.Room;
 import com.yanolja.scbj.domain.hotelRoom.entity.RoomTheme;
+import com.yanolja.scbj.domain.hotelRoom.repository.HotelRoomImageRepository;
+import com.yanolja.scbj.domain.hotelRoom.repository.HotelRoomRepository;
+import com.yanolja.scbj.domain.hotelRoom.repository.RoomThemeRepository;
 import com.yanolja.scbj.domain.member.entity.Authority;
 import com.yanolja.scbj.domain.member.entity.Member;
 import com.yanolja.scbj.domain.member.entity.YanoljaMember;
+import com.yanolja.scbj.domain.member.repository.MemberRepository;
+import com.yanolja.scbj.domain.member.repository.YanoljaMemberRepository;
+import com.yanolja.scbj.domain.member.service.MemberService;
+import com.yanolja.scbj.domain.paymentHistory.service.PaymentService;
 import com.yanolja.scbj.domain.product.dto.request.ProductSearchRequest;
 import com.yanolja.scbj.domain.product.dto.response.ProductSearchResponse;
 import com.yanolja.scbj.domain.product.entity.Product;
 import com.yanolja.scbj.domain.reservation.entity.Reservation;
-import com.yanolja.scbj.global.config.QuerydslConfig;
-import jakarta.persistence.EntityManager;
+import com.yanolja.scbj.domain.reservation.repository.ReservationRepository;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.IntStream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 
-@DataJpaTest
-@Import(QuerydslConfig.class)
+@SpringBootTest
 public class ProductSearchRepositoryTest {
 
-
-
     @Autowired
-    private TestEntityManager entityManager;
+    private PaymentService paymentService;
 
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private HotelRoomRepository hotelRoomRepository;
+
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    @Autowired
+    private RoomThemeRepository roomThemeRepository;
+
+    @Autowired
+    private YanoljaMemberRepository yanoljaMemberRepository;
+
+    @Autowired
+    private HotelRoomImageRepository hotelRoomImageRepository;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    private List<Member> memberList = new ArrayList<>();
+
+    private int threadSize = 11;
+
     private Member createMember(String email, String phoneNumber) {
+
         Member member = Member.builder()
             .email(email)
             .password("password")
@@ -50,8 +88,10 @@ public class ProductSearchRepositoryTest {
             .phone(phoneNumber)
             .authority(Authority.ROLE_USER)
             .build();
-        entityManager.persist(member);
-        return member;
+
+        Member savedMember = memberRepository.save(member);
+        memberList.add(savedMember);
+        return savedMember;
     }
 
     private RoomTheme createRoomTheme(Boolean parking, Boolean pool) {
@@ -61,7 +101,7 @@ public class ProductSearchRepositoryTest {
             .pool(pool)
             .oceanView(false)
             .build();
-        entityManager.persist(roomTheme);
+        roomThemeRepository.save(roomTheme);
         return roomTheme;
     }
 
@@ -84,7 +124,7 @@ public class ProductSearchRepositoryTest {
             .room(room)
             .build();
 
-        entityManager.persist(hotel);
+        hotelRoomRepository.save(hotel);
         return hotel;
     }
 
@@ -93,7 +133,7 @@ public class ProductSearchRepositoryTest {
             .hotel(hotel)
             .url("http://example.com/hotel-room-image.jpg")
             .build();
-        entityManager.persist(hotelRoomImage);
+        hotelRoomImageRepository.save(hotelRoomImage);
         return hotelRoomImage;
     }
 
@@ -102,12 +142,12 @@ public class ProductSearchRepositoryTest {
         YanoljaMember yanoljaMember = YanoljaMember.builder()
             .email(email)
             .build();
-        entityManager.persist(yanoljaMember);
+        yanoljaMemberRepository.save(yanoljaMember);
         return yanoljaMember;
     }
 
     private Reservation createReservation(Hotel hotel, YanoljaMember yanoljaMember,
-                                          LocalDate checkIn, LocalDate checkOut, int purchasePrice) {
+        LocalDate checkIn, LocalDate checkOut, int purchasePrice) {
         Reservation reservation = Reservation.builder()
             .hotel(hotel)
             .yanoljaMember(yanoljaMember)
@@ -115,11 +155,12 @@ public class ProductSearchRepositoryTest {
             .endDate(checkOut.atStartOfDay())
             .purchasePrice(purchasePrice)
             .build();
-        entityManager.persist(reservation);
+        reservationRepository.save(reservation);
         return reservation;
     }
 
-    private Product createProduct(Member member, Reservation reservation, int firstPrice, int secondPrice, int time) {
+    private Product createProduct(Member member, Reservation reservation, int firstPrice,
+        int secondPrice, int time) {
         Product product = Product.builder()
             .reservation(reservation)
             .member(member)
@@ -129,59 +170,27 @@ public class ProductSearchRepositoryTest {
             .secondPrice(secondPrice)
             .secondGrantPeriod(time)
             .build();
-        entityManager.persist(product);
+        productRepository.save(product);
         return product;
     }
 
+
     @BeforeEach
     void init() {
-        IntStream.rangeClosed(1, 10)
-            .forEach(i -> {
-                String randomAddress = "이천";
-                Member member = createMember("user" + i + "@example.com", "홍길동" + i);
-                RoomTheme roomTheme = createRoomTheme(true,true);
-                Hotel hotel2 = createHotel(roomTheme, randomAddress, 3);
-                createHotelRoomImage(hotel2);
-                YanoljaMember yanoljaMember = createYanoljaMember("yanolja" + i + "@example.com");
-                Reservation reservation = createReservation(hotel2, yanoljaMember, LocalDate.now().plusDays(1),
-                    LocalDate.now().plusDays(5),200000);
-                Product product = createProduct(member, reservation, 100000,50000,0);
-               entityManager.flush();
-            });
-
-
-        IntStream.rangeClosed(1, 5)
-            .forEach(i -> {
-                String randomAddress = "강릉";
-                Member member = createMember("user" + i + 20 + "@example.com", "홍길동" + i + 20);
-                RoomTheme roomTheme = createRoomTheme(false,false);
-                Hotel hotel2 = createHotel(roomTheme, randomAddress, 2);
-                createHotelRoomImage(hotel2);
-                YanoljaMember yanoljaMember =
-                    createYanoljaMember("yanolja" + i + 20 + "@example.com");
-                Reservation reservation =
-                    createReservation(hotel2, yanoljaMember, LocalDate.now().plusDays(6),
-                        LocalDate.now().plusDays(8),300000);
-                Product product = createProduct(member, reservation, 200000, 100000,1);
-                entityManager.flush();
-            });
-
-        IntStream.rangeClosed(1, 5)
+        IntStream.rangeClosed(1, threadSize - 1)
             .forEach(i -> {
                 String randomAddress = "서울";
                 Member member = createMember("user" + i + 30 + "@example.com", "홍길동" + i + 30);
-                RoomTheme roomTheme = createRoomTheme(false,false);
-                Hotel hotel2 = createHotel(roomTheme, randomAddress, 2);
+                RoomTheme roomTheme = createRoomTheme(true, true);
+                Hotel hotel2 = createHotel(roomTheme, randomAddress, 3);
                 createHotelRoomImage(hotel2);
                 YanoljaMember yanoljaMember =
                     createYanoljaMember("yanolja" + i + 30 + "@example.com");
                 Reservation reservation =
                     createReservation(hotel2, yanoljaMember, LocalDate.now().plusDays(8),
-                        LocalDate.now().plusDays(13),400000);
-                Product product = createProduct(member, reservation, 300000, 250000,0);
-                entityManager.flush();
+                        LocalDate.now().plusDays(13), 400000);
+                Product product = createProduct(member, reservation, 300000, 250000, 0);
             });
-        entityManager.clear();
     }
 
 
@@ -190,131 +199,130 @@ public class ProductSearchRepositoryTest {
     class Context_searchProduct {
 
         @Test
-        @DisplayName("서울을 선택하면 서울에 관한 숙박업소가 보인다")
-        public void will_success_testAreaSearchProduct() {
+        @DisplayName("판매중인것만 조회가 된다")
+        public void will_success_get_on_sale() {
             //given
-            ProductSearchRequest searchRequest = ProductSearchRequest.builder()
-                .location("서울")
-                .build();
+            long startTime = System.currentTimeMillis();
 
-            // when
-            Page<ProductSearchResponse> results =
-                productRepository.search(PageRequest.of(0, 1), searchRequest);
+            ProductSearchRequest productSearchRequest = ProductSearchRequest.builder().build();
 
-            // then
-            assertThat(results).isNotEmpty();
-            List<ProductSearchResponse> responses = results.getContent();
-            for (ProductSearchResponse respons : responses) {
-                System.err.println(respons.getOriginalPrice());
+            //when
+            Page<ProductSearchResponse> responses =
+                productRepository.search(PageRequest.of(0, 10), productSearchRequest);
+
+            //then
+
+            long endTime = System.currentTimeMillis();
+            System.out.println("시간:" + endTime);
+        }
+    }
+
+
+    @Nested
+    @DisplayName("전체조회 테스트는")
+    class Context_searchProductforPayment {
+
+        @Test
+        @DisplayName("락을 이용하지 않는경우, 상품 전체 조회가 더 짧게 걸린다.") //상품전체조회 시간 테스트
+        public void _will_success_withOutLock() throws Exception {
+            long startTime = System.currentTimeMillis();
+            ExecutorService executorService = Executors.newFixedThreadPool(threadSize);
+            CountDownLatch latch = new CountDownLatch(threadSize);
+
+
+            String key1 = "kakaoPay" + memberList.get(0).getId();
+
+            Map<String, String> map = new HashMap<>();
+            map.put("productId", "1");
+            map.put("customerName", "asdasd");
+            map.put("customerEmail", "email.com");
+            map.put("customerPhoneNumber", "gasdas");
+            map.put("price", "3400");
+            map.put("tid", "gasda");
+            HashOperations<String, String, String> hashOperations = redisTemplate.opsForHash();
+            hashOperations.putAll(key1, map);
+
+            ProductSearchRequest productSearchRequest = ProductSearchRequest.builder().build();
+            executorService.submit(() ->{
+                try {
+                    Page<ProductSearchResponse> responses =
+                        productRepository.search(PageRequest.of(0, 10), productSearchRequest);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    latch.countDown();
+                }
+            });
+
+            for (int i = 0; i < threadSize; i++) {
+                executorService.submit(() ->{
+                    try {
+                        paymentService.orderProductWithOutLock("asg", memberList.get(0).getId());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        latch.countDown();
+                    }
+                });
             }
-            assertThat(responses.size()).isEqualTo(5);
+
+            latch.await();
+            long endTime = System.currentTimeMillis();
+
+            System.err.println("시간: " + (endTime - startTime));
         }
 
         @Test
-        @DisplayName("날짜를 선택하면 체크인 날짜를 기준으로 숙박업소가 보인다")
-        public void will_success_testDateSearchProduct() {
-            //given
-            ProductSearchRequest searchRequest =
-                ProductSearchRequest.builder().checkIn(LocalDate.now())
-                    .checkOut(LocalDate.now().plusDays(5)).build();
+        @DisplayName("락을 이용하는 경우, 상품 전체 조회가 더 오래걸린다.")
+        void _will_success_withLock() throws Exception {
+            long startTime = System.currentTimeMillis();
+            ExecutorService executorService = Executors.newFixedThreadPool(threadSize);
+            CountDownLatch latch = new CountDownLatch(threadSize);
 
-            //when
 
-            Page<ProductSearchResponse> results =
-                productRepository.search(PageRequest.of(0, 10), searchRequest);
+            String key1 = "kakaoPay" + memberList.get(0).getId();
 
-            //then
-            assertThat(results).isNotEmpty();
-            List<ProductSearchResponse> content = results.getContent();
-            for (ProductSearchResponse productSearchResponse : content) {
-                System.out.println(productSearchResponse.getOriginalPrice());
+            Map<String, String> map = new HashMap<>();
+            map.put("productId", "1");
+            map.put("customerName", "asdasd");
+            map.put("customerEmail", "email.com");
+            map.put("customerPhoneNumber", "gasdas");
+            map.put("price", "3400");
+            map.put("tid", "gasda");
+
+            HashOperations<String, String, String> hashOperations = redisTemplate.opsForHash();
+            hashOperations.putAll(key1, map);
+
+            ProductSearchRequest productSearchRequest = ProductSearchRequest.builder().build();
+
+            executorService.submit(() ->{
+                try {
+                    Page<ProductSearchResponse> responses =
+                        productRepository.search(PageRequest.of(0, 10), productSearchRequest);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    latch.countDown();
+                }
+            });
+
+            for (int i = 0; i < threadSize; i++) {
+                executorService.submit(() ->{
+                    try {
+                        paymentService.orderProductWithLock("asg", memberList.get(0).getId());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        latch.countDown();
+                    }
+                });
             }
 
-            assertThat(content.size()).isEqualTo(10);
-        }
 
-        @Test
-        @DisplayName("인원을 설정하면 해당 최대인원의 상품들이 조회되야한다")
-        public void will_success_testMaximumPeopleProduct() {
-            //given
-            ProductSearchRequest searchRequest = ProductSearchRequest.builder()
-                .quantityPeople(3)
-                .build();
+            latch.await();
+            long endTime = System.currentTimeMillis();
 
-            //when
-            Page<ProductSearchResponse> results =
-                productRepository.search(PageRequest.of(0, 10), searchRequest);
-
-
-            //then
-            assertThat(results).isNotEmpty();
-            for (ProductSearchResponse result : results) {
-                System.out.println(result.getOriginalPrice());
-
-            }
-            List<ProductSearchResponse> content = results.getContent();
-            assertThat(content.size()).isEqualTo(10);
-        }
-
-        @Test
-        @DisplayName("테마를 통해 상품을 조회한다")
-        public void will_success_testThemeSearch() {
-            //given
-            ProductSearchRequest searchParkingRequest = ProductSearchRequest.builder()
-                .parking(true)
-                .build();
-
-            ProductSearchRequest searchPoolRequest = ProductSearchRequest.builder()
-                .pool(true)
-                .build();
-
-            //when
-            Page<ProductSearchResponse> parkingResults =
-                productRepository.search(PageRequest.of(0, 10), searchParkingRequest);
-
-            Page<ProductSearchResponse> poolResults =
-                productRepository.search(PageRequest.of(0, 10), searchPoolRequest);
-
-            //then
-            assertThat(parkingResults).isNotEmpty();
-            assertThat(poolResults).isNotEmpty();
-            List<ProductSearchResponse> parkingProduct = parkingResults.getContent();
-            List<ProductSearchResponse> poolProduct = poolResults.getContent();
-            assertThat(parkingProduct.size()).isEqualTo(10);
-            assertThat(poolProduct.size()).isEqualTo(10);
-
-        }
-
-        @Test
-        @DisplayName("정렬을 통해서 조회한다")
-        public void will_success() {
-            //given
-            ProductSearchRequest highSaleSearchRequest = ProductSearchRequest.builder()
-                .sorted("높은 할인 순")
-                .build();
-
-            ProductSearchRequest lowPriceRequest = ProductSearchRequest.builder()
-                .sorted("낮은 가격 순")
-                .build();
-
-            //when
-            Page<ProductSearchResponse> highSearchResult =
-                productRepository.search(PageRequest.of(0, 10), highSaleSearchRequest);
-
-            Page<ProductSearchResponse> lowPriceResult =
-                productRepository.search(PageRequest.of(0, 10), lowPriceRequest);
-            //then
-            assertThat(highSearchResult).isNotEmpty();
-            List<ProductSearchResponse> content = highSearchResult.getContent();
-
-            for (int i = 0; i < 5; i++) {
-                assertThat(content.get(i).getSalePercentage()).isEqualTo(0.5);
-            }
-
-            assertThat(lowPriceResult).isNotEmpty();
-            for (int i = 0; i < 10; i++) {
-                assertThat(lowPriceResult.getContent().get(i).getSalePrice()).isEqualTo(100000);
-            }
+            System.err.println("시간: " + (endTime - startTime));
         }
     }
 }
