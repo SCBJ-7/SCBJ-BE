d9453eed9fef1ed97aac9869d59c5f692911e86b
kwonMInwoo <qwerty029369@naver.com>
2024-01-18 19:27:34 +0900

    feat: 정산 스케줄링, 알람 보내기 로직
    - 자정에 정산 체크 후 정산
    - 알람 보내기 로직 추가

--- src/main/java/com/yanolja/scbj/domain/paymentHistory/service/paymentApi/KaKaoPaymentService.java
+++ src/main/java/com/yanolja/scbj/domain/paymentHistory/service/paymentApi/KaKaoPaymentService.java
@@ -1,5 +1,6 @@
 package com.yanolja.scbj.domain.paymentHistory.service.paymentApi;
 
+import com.yanolja.scbj.domain.alarm.service.AlarmService;
 import com.yanolja.scbj.domain.hotelRoom.entity.Hotel;
 import com.yanolja.scbj.domain.member.entity.Member;
 import com.yanolja.scbj.domain.member.exception.MemberNotFoundException;
@@ -17,12 +18,14 @@ import com.yanolja.scbj.domain.product.exception.ProductNotFoundException;
 import com.yanolja.scbj.domain.product.repository.ProductRepository;
 import com.yanolja.scbj.domain.reservation.entity.Reservation;
 import com.yanolja.scbj.global.config.RetryConfig;
+import com.yanolja.scbj.global.config.fcm.FCMRequest.Data;
 import com.yanolja.scbj.global.exception.ErrorCode;
 import com.yanolja.scbj.global.exception.InternalServerException;
 import com.yanolja.scbj.global.util.TimeValidator;
 import jakarta.annotation.PostConstruct;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
@@ -58,6 +61,7 @@ public class KaKaoPaymentService implements PaymentApiService {
     private final ProductRepository productRepository;
     private final MemberRepository memberRepository;
     private final PaymentHistoryRepository paymentHistoryRepository;
+    private final AlarmService alarmService;
     private final RedisTemplate<String, String> redisTemplate;
     private final RestTemplate restTemplate;
 
@@ -101,6 +105,7 @@ public class KaKaoPaymentService implements PaymentApiService {
         }
 
         String productName = targetHotel.getHotelName() + " " + targetHotel.getRoom().getRoomName();
+
         MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
         params.add("cid", "TC0ONETIME");
         params.add("partner_order_id", String.valueOf(productId));
@@ -130,6 +135,7 @@ public class KaKaoPaymentService implements PaymentApiService {
             redisMap.put("customerName", paymentReadyRequest.customerName());
             redisMap.put("customerEmail", paymentReadyRequest.customerEmail());
             redisMap.put("customerPhoneNumber", paymentReadyRequest.customerPhoneNumber());
+            redisMap.put("productName", productName);
 
             HashOperations<String, String, String> hashOperations = redisTemplate.opsForHash();
             String key = KEY_PREFIX + memberId;
@@ -159,6 +165,7 @@ public class KaKaoPaymentService implements PaymentApiService {
             .get(key, "customerPhoneNumber");
         String price = (String) redisTemplate.opsForHash().get(key, "price");
         String tid = (String) redisTemplate.opsForHash().get(key, "tid");
+        String productName = (String) redisTemplate.opsForHash().get(key, "productName");
 
         MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
         params.add("cid", "TC0ONETIME");
@@ -197,7 +204,10 @@ public class KaKaoPaymentService implements PaymentApiService {
                 .build();
 
             product.sell();
-            paymentHistoryRepository.save(paymentHistory);
+            PaymentHistory savedPaymentHistory = paymentHistoryRepository.save(paymentHistory);
+
+            alarmService.createAlarm(product.getMember().getId(), savedPaymentHistory.getId(),
+                new Data("판매완료", productName + "의 판매가 완료되었어요!", LocalDateTime.now()));
 
         } catch (RestClientException | URISyntaxException e) {
             throw new KakaoPayException(ErrorCode.KAKAO_PAY_INFO_FAIL);
@@ -236,7 +246,7 @@ public class KaKaoPaymentService implements PaymentApiService {
         }
     }
 
-    private void checkStatus(HttpStatusCode statusCode){
+    private void checkStatus(HttpStatusCode statusCode) {
         if (statusCode.equals(HttpStatus.BAD_REQUEST) || statusCode.equals(
             HttpStatus.UNAUTHORIZED) || statusCode.equals(HttpStatus.FORBIDDEN)
             || statusCode.equals(HttpStatus.NOT_FOUND)) {
@@ -252,7 +262,7 @@ public class KaKaoPaymentService implements PaymentApiService {
     }
 
     @Recover
-    private void sendExceptionForPayFailure(KakaoPayException e){
+    private void sendExceptionForPayFailure(KakaoPayException e) {
         throw new KakaoPayException(e.getErrorCode());
     }
 
