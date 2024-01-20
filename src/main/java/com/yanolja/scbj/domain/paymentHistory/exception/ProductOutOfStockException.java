bbc81fa49ca3d8ca4911339ec0f4f4bc3d134f87
kwonMInwoo <qwerty029369@naver.com>
2024-01-20 14:43:40 +0900

    feat: 재고 부족 예외 추가
    - 재고 부족 시 발생하는 예외 추가

--- src/main/java/com/yanolja/scbj/domain/paymentHistory/exception/ProductOutOfStockException.java
+++ src/main/java/com/yanolja/scbj/domain/paymentHistory/exception/ProductOutOfStockException.java
@@ -1,2 +1,10 @@
-package com.yanolja.scbj.domain.paymentHistory.exception;public class ProductOutOfException {
+package com.yanolja.scbj.domain.paymentHistory.exception;
+
+import com.yanolja.scbj.global.exception.ApplicationException;
+import com.yanolja.scbj.global.exception.ErrorCode;
+
+public class ProductOutOfStockException extends ApplicationException {
+    public ProductOutOfStockException(ErrorCode errorCode) {
+        super(errorCode);
+    }
 }
