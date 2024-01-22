ad16d17b8c9784a27117275937c53e927e406353
Yurim Yang <yang980329@likelion.org>
2024-01-22 09:46:10 +0900

    fix: stock 개수 상수화

--- src/main/java/com/yanolja/scbj/domain/product/service/ProductService.java
+++ src/main/java/com/yanolja/scbj/domain/product/service/ProductService.java
@@ -46,6 +46,7 @@ public class ProductService {
     private final WeekendDtoConverter weekendDtoConverter;
 
     private static final int MIN_SECOND_GRANT_PERIOD = 3;
+    private static final int FIXED_STOCK = 1;
 
     @Transactional
     public ProductPostResponse postProduct(Long memberId, Long reservationId,
@@ -89,7 +90,7 @@ public class ProductService {
             .bank(productPostRequest.bank())
             .accountNumber(productPostRequest.accountNumber())
             .secondGrantPeriod(productPostRequest.secondGrantPeriod())
-            .stock(1).build();
+            .stock(FIXED_STOCK).build();
 
         Product savedProduct = productRepository.save(product);
 
