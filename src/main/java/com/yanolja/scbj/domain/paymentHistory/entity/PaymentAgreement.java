79e3d77db40e8516d5724daeb0a5739730d59457
kwonMInwoo <qwerty029369@naver.com>
2024-01-22 16:07:42 +0900

    feat: 컬럼의 기본 값 추가
    - 기본 값으로 false가 들어가게 수정

--- src/main/java/com/yanolja/scbj/domain/paymentHistory/entity/PaymentAgreement.java
+++ src/main/java/com/yanolja/scbj/domain/paymentHistory/entity/PaymentAgreement.java
@@ -9,6 +9,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.Comment;
 
 @Entity
@@ -22,22 +23,27 @@ public class PaymentAgreement {
     private Long id;
 
     @Column(nullable = false)
+    @ColumnDefault(value = "false")
     @Comment("만 14세 이상 이용 동의")
     private boolean isAgeOver14;
 
     @Column(nullable = false)
+    @ColumnDefault(value = "false")
     @Comment("이용 규칙 동의")
     private boolean useAgree;
 
     @Column(nullable = false)
+    @ColumnDefault(value = "false")
     @Comment("취소 및 환불 규칙 동의")
     private boolean cancelAndRefund;
 
     @Column(nullable = false)
+    @ColumnDefault(value = "false")
     @Comment("개인정보 수집 및 이용 동의")
     private boolean collectPersonalInfo;
 
     @Column(nullable = false)
+    @ColumnDefault(value = "false")
     @Comment("개인정보 제 3자 제공 동의")
     private boolean thirdPartySharing;
 
