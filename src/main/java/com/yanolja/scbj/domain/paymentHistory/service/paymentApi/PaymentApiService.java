7dd5eaafdc8f03f3880fdb7cdd4dc1627d41627b
kwonMInwoo <qwerty029369@naver.com>
2024-01-22 21:36:00 +0900

    test: 결제 테스트 작성
    - 결제 로직 테스트 코드 작성 완료

--- src/main/java/com/yanolja/scbj/domain/paymentHistory/service/paymentApi/PaymentApiService.java
+++ src/main/java/com/yanolja/scbj/domain/paymentHistory/service/paymentApi/PaymentApiService.java
@@ -1,10 +1,11 @@
 package com.yanolja.scbj.domain.paymentHistory.service.paymentApi;
 
 import com.yanolja.scbj.domain.paymentHistory.dto.request.PaymentReadyRequest;
+import com.yanolja.scbj.domain.paymentHistory.dto.response.PreparePaymentResponse;
 
 public interface PaymentApiService {
 
-    String preparePayment(Long memberId, Long productId, PaymentReadyRequest paymentReadyRequest);
+    PreparePaymentResponse preparePayment(Long memberId, Long productId, PaymentReadyRequest paymentReadyRequest);
 
     void approvePaymentWithLock(String pgToken, Long memberId);
 
