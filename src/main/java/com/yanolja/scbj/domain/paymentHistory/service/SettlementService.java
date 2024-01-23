0ffdc9dc879c4b13d8e06745dbdac5ea3e493d0a
kwonMInwoo <qwerty029369@naver.com>
2024-01-23 15:11:04 +0900

    feat: 벌크 연산 도입
    - update 쿼리를 벌크 연산으로 최적화

--- src/main/java/com/yanolja/scbj/domain/paymentHistory/service/SettlementService.java
+++ src/main/java/com/yanolja/scbj/domain/paymentHistory/service/SettlementService.java
@@ -3,8 +3,8 @@ package com.yanolja.scbj.domain.paymentHistory.service;
 import com.yanolja.scbj.domain.alarm.service.AlarmService;
 import com.yanolja.scbj.domain.paymentHistory.entity.PaymentHistory;
 import com.yanolja.scbj.domain.paymentHistory.repository.PaymentHistoryRepository;
-import com.yanolja.scbj.global.config.SchedulerConfig;
 import com.yanolja.scbj.global.config.fcm.FCMRequest.Data;
+import jakarta.persistence.EntityManager;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +18,7 @@ public class SettlementService {
 
     private final PaymentHistoryRepository paymentHistoryRepository;
     private final AlarmService alarmService;
+    private final EntityManager entityManager;
 
     @Scheduled(cron = "${schedule.cron}")
     @Transactional
@@ -25,8 +26,15 @@ public class SettlementService {
         List<PaymentHistory> targetPaymentHistoryList
             = paymentHistoryRepository.findPaymentHistoriesWithNotSettlement();
 
+        String updateQuery =
+            "update PaymentHistory ph " +
+                "set ph.settlement = true " +
+                "where ph.settlement = false";
+
+        entityManager.createQuery(updateQuery)
+            .executeUpdate();
+
         for (PaymentHistory paymentHistory : targetPaymentHistoryList) {
-            paymentHistory.processSettlement();
             Long memberId = paymentHistory.getProduct().getMember().getId();
             alarmService.createAlarm(memberId, paymentHistory.getId(),
                 new Data("정산 완료", paymentHistory.getProductName() + "의 정산이 완료되었습니다.",
