36c9b38380cb34f6edc82a363ca705279caa85e2
kwonMInwoo <qwerty029369@naver.com>
2024-01-23 14:44:05 +0900

    feat: cron 시간 yml로 옮김
    - cron 되는 시간을 가리기 위해 yml에서 가져오도록 변경

--- src/main/java/com/yanolja/scbj/domain/paymentHistory/service/SettlementService.java
+++ src/main/java/com/yanolja/scbj/domain/paymentHistory/service/SettlementService.java
@@ -3,24 +3,24 @@ package com.yanolja.scbj.domain.paymentHistory.service;
 import com.yanolja.scbj.domain.alarm.service.AlarmService;
 import com.yanolja.scbj.domain.paymentHistory.entity.PaymentHistory;
 import com.yanolja.scbj.domain.paymentHistory.repository.PaymentHistoryRepository;
+import com.yanolja.scbj.global.config.SchedulerConfig;
 import com.yanolja.scbj.global.config.fcm.FCMRequest.Data;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class SettlementService {
 
-    private final String CRON_EXPRESSION = "0 0 0 * * ?";
-
     private final PaymentHistoryRepository paymentHistoryRepository;
     private final AlarmService alarmService;
 
-
-    @Scheduled(cron = CRON_EXPRESSION)
+    @Scheduled(cron = "${schedule.cron}")
+    @Transactional
     public void settlementPaymentHistorySchedule() {
         List<PaymentHistory> targetPaymentHistoryList
             = paymentHistoryRepository.findPaymentHistoriesWithNotSettlement();
@@ -28,7 +28,6 @@ public class SettlementService {
         for (PaymentHistory paymentHistory : targetPaymentHistoryList) {
             paymentHistory.processSettlement();
             Long memberId = paymentHistory.getProduct().getMember().getId();
-
             alarmService.createAlarm(memberId, paymentHistory.getId(),
                 new Data("정산 완료", paymentHistory.getProductName() + "의 정산이 완료되었습니다.",
                     LocalDateTime.now()));
