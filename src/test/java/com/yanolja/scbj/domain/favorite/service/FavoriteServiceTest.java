3a95a3968022428ef1542cd0b4519d566a361ade
Aleexender <alexkjh2011@gmail.com>
2024-05-01 10:38:00 +0900

    Feature: FavoriteService 테스트 추가

---
+++ src/test/java/com/yanolja/scbj/domain/favorite/service/FavoriteServiceTest.java
@@ -0,0 +1,100 @@
+package com.yanolja.scbj.domain.favorite.service;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.yanolja.scbj.domain.like.entity.Favorite;
+import com.yanolja.scbj.domain.like.entity.dto.request.FavoriteRegisterRequest;
+import com.yanolja.scbj.domain.like.entity.dto.response.FavoriteRegisterResponse;
+import com.yanolja.scbj.domain.like.exception.FavoriteDeleteFailException;
+import com.yanolja.scbj.domain.like.repository.FavoriteRepository;
+import com.yanolja.scbj.domain.like.service.FavoriteService;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.dao.DataIntegrityViolationException;
+
+public class FavoriteServiceTest {
+    @Mock
+    private FavoriteRepository favoriteRepository;
+
+    @InjectMocks
+    private FavoriteService favoriteService;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    Favorite setFavorite() {
+        return Favorite.builder()
+            .id(1L)
+            .productId(1L)
+            .memberId(1L)
+            .favoriteStatement(true)
+            .build();
+    }
+
+    @Nested
+    @DisplayName("찜 등록과 삭제는")
+    class Context_Favorite_register_delete {
+        @Test
+        void registerFavorite_success() {
+            // given
+            Long memberId = 1L;
+            Long productId = 1L;
+            FavoriteRegisterRequest request =
+                new FavoriteRegisterRequest(true);
+
+            when(favoriteRepository.save(any(Favorite.class))).thenReturn(setFavorite());
+
+            // when
+            FavoriteRegisterResponse response =
+                favoriteService.register(memberId, productId, request);
+
+            // then
+            assertNotNull(response);
+            verify(favoriteRepository).save(any(Favorite.class));
+        }
+
+        @Test
+        void registerFavorite_fail_dueToDataIntegrityViolation() {
+            // given
+            Long memberId = 1L;
+            Long productId = 1L;
+            FavoriteRegisterRequest request = new FavoriteRegisterRequest(true);
+            when(favoriteRepository.save(any(Favorite.class))).thenThrow(
+                new DataIntegrityViolationException("Violation"));
+
+            // when & then
+            assertThrows(FavoriteDeleteFailException.class, () -> {
+                favoriteService.register(memberId, productId, request);
+            });
+        }
+
+        @Test
+        void deleteFavorite_fail_dueToEntityNotFound() {
+            // given
+            Long memberId = 1L;
+            Long productId = 1L;
+
+
+            //when
+            when(favoriteRepository.findByMemberIdAndProductId(memberId, productId)).thenThrow(
+                new EntityNotFoundException());
+
+            //then
+            assertThrows(FavoriteDeleteFailException.class, () -> {
+                favoriteService.remove(memberId, productId);
+            });
+        }
+    }
+}
