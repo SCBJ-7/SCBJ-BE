16b20fe793d40850aac646f456d3b50887349fac
Aleexender <alexkjh2011@gmail.com>
2024-05-09 02:34:22 +0900

    Feature: Response 추가 및 Mapper 수정

---
+++ src/main/java/com/yanolja/scbj/domain/like/entity/dto/response/FavoritesResponse.java
@@ -0,0 +1,19 @@
+package com.yanolja.scbj.domain.like.entity.dto.response;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.Builder;
+
+public record FavoritesResponse(
+    String hotelName,
+    String roomType,
+    String imageUrl,
+    LocalDateTime checkInDate,
+    LocalDateTime checkOutDate,
+    int price
+) {
+
+    @Builder
+    public FavoritesResponse {
+    }
+}
--- src/main/java/com/yanolja/scbj/domain/like/service/FavoriteMapper.java
+++ src/main/java/com/yanolja/scbj/domain/like/service/FavoriteMapper.java
@@ -1,11 +1,7 @@
 package com.yanolja.scbj.domain.like.service;
 
 import com.yanolja.scbj.domain.like.entity.Favorite;
-import com.yanolja.scbj.domain.like.entity.dto.request.FavoriteRegisterRequest;
 import com.yanolja.scbj.domain.like.entity.dto.response.FavoriteDeleteResponse;
-import com.yanolja.scbj.domain.like.entity.dto.response.FavoriteRegisterResponse;
-import com.yanolja.scbj.domain.member.entity.Member;
-import com.yanolja.scbj.domain.product.entity.Product;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
