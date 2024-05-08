3110ea3d53500a9d9c36a3b94d7086af8ec8e079
Aleexender <alexkjh2011@gmail.com>
2024-05-09 02:33:48 +0900

    Feature: Favorite MVC 추가

--- src/main/java/com/yanolja/scbj/domain/like/repository/FavoriteRepository.java
+++ src/main/java/com/yanolja/scbj/domain/like/repository/FavoriteRepository.java
@@ -1,8 +1,36 @@
 package com.yanolja.scbj.domain.like.repository;
 
 import com.yanolja.scbj.domain.like.entity.Favorite;
+import com.yanolja.scbj.domain.like.entity.dto.response.FavoritesResponse;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FavoriteRepository extends JpaRepository<Favorite,Long> {
     Favorite findByMemberIdAndProductId(Long memberId, Long productId);
+
+    @Query("""
+    SELECT new com.yanolja.scbj.domain.like.entity.dto.response.FavoritesResponse(
+        h.hotelName,
+        h.room.bedType,
+        hImg.url,
+        res.startDate,
+        res.endDate,
+        CASE
+            WHEN (p.secondPrice <> 0 AND
+                  (p.secondGrantPeriod <> 0) AND
+                  function('DATE_SUB', res.startDate, function('INTERVAL', p.secondGrantPeriod, 'HOUR')) < function('NOW'))
+            THEN p.secondPrice
+            ELSE p.firstPrice
+        END
+    )
+    FROM Favorite f
+    JOIN Product p ON f.productId = p.id
+    JOIN Reservation res ON p.reservation.id = res.id
+    JOIN Hotel h ON res.hotel.id = h.id
+    LEFT JOIN HotelRoomImage hImg ON hImg.hotel.id = h.id
+    WHERE f.memberId = :memberId
+    """)
+    List<FavoritesResponse> findFavoritesByMemberId(@Param("memberId") Long memberId);
 }
