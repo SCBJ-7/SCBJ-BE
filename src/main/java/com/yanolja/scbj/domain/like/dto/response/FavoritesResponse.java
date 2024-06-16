5c83de8304f9fa0c3c2e2f0452935fbbc9aeda87
Aleexender <alexkjh2011@gmail.com>
2024-06-17 00:30:59 +0900

    fix: 쓸때없는 import문 제거

--- src/main/java/com/yanolja/scbj/domain/like/dto/response/FavoritesResponse.java
+++ src/main/java/com/yanolja/scbj/domain/like/dto/response/FavoritesResponse.java
@@ -1,15 +1,19 @@
 package com.yanolja.scbj.domain.like.dto.response;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import lombok.Builder;
 
 public interface FavoritesResponse {
     Long getId();
+
     String getHotelName();
+
     String getBedType();
+
     String getImageUrl();
+
     LocalDateTime getCheckInDate();
+
     LocalDateTime getCheckOutDate();
+
     int getPrice();
 }
